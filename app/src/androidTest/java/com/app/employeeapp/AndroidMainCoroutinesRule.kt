ccf424921da64d55bb6a7df423caa9fb7a53762e
GhanshyamD <gshyam.aman@gmail.com>
2022-05-06 00:15:33 +0100

    updated code commit

---
+++ app/src/androidTest/java/com/app/employeeapp/AndroidMainCoroutinesRule.kt
@@ -0,0 +1,24 @@
+package com.app.employeeapp
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.*
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+@ExperimentalCoroutinesApi
+class AndroidMainCoroutinesRule(
+    private val dispatcher: CoroutineDispatcher = StandardTestDispatcher()
+): TestWatcher(), TestCoroutineScope by TestCoroutineScope(dispatcher) {
+
+    override fun starting(description: Description) {
+        super.starting(description)
+        Dispatchers.setMain(dispatcher)
+    }
+
+    override fun finished(description: Description) {
+        super.finished(description)
+        cleanupTestCoroutines()
+        Dispatchers.resetMain()
+    }
+}
