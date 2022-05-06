a8f50b5c6672fedd48b7aad9edc7ae241ece3008
GhanshyamD <gshyam.aman@gmail.com>
2022-05-06 01:14:00 +0100

    Updated the unit test

--- app/src/androidTest/java/com/app/employeeapp/ui/employee/detail/DetailFragmentTest.kt
+++ app/src/androidTest/java/com/app/employeeapp/ui/employee/detail/DetailFragmentTest.kt
@@ -1,21 +1,14 @@
 package com.app.employeeapp.ui.employee.detail
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.core.os.bundleOf
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.test.espresso.Espresso
-import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.filters.MediumTest
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import com.app.employeeapp.AndroidMainCoroutinesRule
-import com.app.employeeapp.R
 import com.app.employeeapp.launchFragmentInHiltContainer
 import com.app.employeeapp.model.Detail
-import com.app.employeeapp.model.Employee
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
